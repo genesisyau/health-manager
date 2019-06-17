04897bbb476637fed9377f166190748964187fd1
drosaslin <davidrosas29@hotmail.com>
2019-06-18 03:38:46 +0800

    firestore responses are now parsed with GSON

--- MyDrugJournal/app/src/main/java/com/example/android/mydrugjournal/data/Date.java
+++ MyDrugJournal/app/src/main/java/com/example/android/mydrugjournal/data/Date.java
@@ -6,21 +6,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Date {
-    private String year;
-    private String month;
     private String day;
     private String time;
 
     public Date() {
-        year = "";
-        month = "";
         day = "";
         time = "";
     }
 
-    public Date(String year, String month, String day, String time) {
-        this.year = year;
-        this.month = month;
+    public Date(String day, String time) {
         this.day = day;
         this.time = time;
     }
