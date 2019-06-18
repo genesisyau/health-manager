292e8b925fcc8845a84a5072a649de434de94ab8
Genesis Yau <genesisyau@hotmail.com>
2019-06-18 14:32:53 +0800

    Added send feedback email functionality

--- MyDrugJournal/app/src/main/java/com/example/android/mydrugjournal/fragments/AboutFragment.java
+++ MyDrugJournal/app/src/main/java/com/example/android/mydrugjournal/fragments/AboutFragment.java
@@ -1,12 +1,15 @@
 package com.example.android.mydrugjournal.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.android.mydrugjournal.R;
 
@@ -21,5 +24,20 @@ public class AboutFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Button contactBtn = getView().findViewById(R.id.contactUsBtn);
+        contactBtn.setOnClickListener(onContactUsClick);
     }
+
+    private View.OnClickListener onContactUsClick = view -> {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/rfc822");
+        i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"gyauxd@gmail.com"});
+        i.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+        i.putExtra(Intent.EXTRA_TEXT   , "");
+        try {
+            startActivity(Intent.createChooser(i, "Send mail..."));
+        } catch (android.content.ActivityNotFoundException ex) {
+            Log.d("contact us", ex.toString());
+        }
+    };
 }
